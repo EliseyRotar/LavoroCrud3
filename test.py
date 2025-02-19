969598a6882622df23b848453496e4d68cdec175
EliseyRotar <nutellaelik@gmail.com>
2025-02-19 19:01:47 +0100

    Adding some functions

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+import os
+import time
+import winsound
 #Programma crea la tua scheda allenamento (timer)
 #Crea tipo allenamento
 #Numero di esercizi 
@@ -7,41 +10,69 @@
 #PROBLEMI: eccezzioni, numeri scritti in parole, maiuscole
 allenamento1 = {
     "GruppoAllenante":"Schiena",
-    "N°Esercizi":0,
+    "NumEsercizi":0,
     "TipoAllenamento":"Timer", #timer/senza timer/misto
     "TempoRiscaldamento":0, #secondi
     "TempoRipresa":0 #secondi
-
 }
-
-allenamenti = [allenamento1]
 
 esercizio1 = {
     "TipologiaEsercizio":"Timer", #timer/senza timer
     "TempoAllenamento":10,
-    "Serie":0
+    "Serie":2
 }
 
-esercizi = [esercizio1]
+esercizio2 = {
+    "TipologiaEsercizio":"Timer", #timer/senza timer
+    "TempoAllenamento":50,
+    "Serie":1
+}
+esercizi = [esercizio1,esercizio2]
+allenamenti = [allenamento1]
+Scheda2 = []
+Scheda1= [allenamento1, esercizio1, esercizio2]
 
 def creaEsercizio(esercizio):
+    
     esercizio = {
         "TipologiaEsercizio":tipoEsercizio,
         "TempoAllenamento":tempoAllenamento,
         "Serie": numeroSerie
     }
-    tipoEsercizio = input("Inserire la parola timer se si vuole avere un timer per questo esercizio inserire la parola  ")
-    tempoAllenamento = int(input("Inserisci il tempo richiesto per questo esercizio: "))
+    tipoEsercizio = int(input("Inserire 1 se si vuole avere un timer per questo esercizio, invece inserire qualsiasi altra cosa se non si vuole avere un timer: "))
+    if tipoEsercizio == 1:
+        tempoAllenamento = int(input("Inserisci il tempo richiesto per questo esercizio: "))
     numeroSerie = int(input("Inserire il numero di serie: "))
 
 def creaAllenamento(allenamento):
     allenamento = {
         "GruppoAllenante":gruppoAllenante,
-        "N°Esercizi":nEsercizi,
-        "TipoAllenamento":"Timer",  
-        "TempoRiscaldamento":0, 
-        "TempoRipresa":0 
+        "NumEsercizi":nEsercizi,
+        "TipoAllenamento":tipoAllenamento,  
+        "TempoRiscaldamento":tempoRiscaldamento, 
+        "TempoRiposo":tempoRiposo
     }
     gruppoAllenante = input("Inserire il gruppo o i gruppi muscolari che si vogliono allenare: ")
     nEsercizi = int(input("Inserisci il numero di esercizi contenuti in questo allenamento: "))
-    tipoAllenamento = (input("Inserire il numero di serie: "))
+    tipoAllenamento = input("Inserire 1 se è un allenamento contenente solo esercizi con timer, inserire 2 se non vuoi i timer, inserire 3 se è un allenamento sia con timer che senza.")
+    tempoRiscaldamento = int(input("Inserire quanto tempo bisogna riservare per il riscaldamento: "))
+    tempoRiposo = int(input("Inserisci il tempo di riposo tra ogni serie"))
+
+def creaSuperSet(superset):
+    set1 = input("Inserisci il nome del primo set: ")
+    set2 = input("Inserisci il nome del secondo set: ")
+    superset = [set1, set2]
+    creaEsercizio(set1)
+    creaEsercizio(set2)
+
+def vediEsercizio(esercizio):
+    #print(f'{"Tipologia:":<15}{"Durata:":<15}{"N° serie:":<15}')
+    print(f'{esercizio["TipologiaEsercizio"]:<15}{esercizio["TempoAllenamento"]:<15}{esercizio["Serie"]:<15}')
+
+def vediScheda(scheda):
+    print(scheda["GruppoAllenante"]) #nome !!!
+    print(f'{"Tipologia:":<15}{"Durata:":<15}{"N° serie:":<15}')
+    count = 0
+    while count < len(scheda):
+        vediEsercizio(scheda[count])
+        count+=1
